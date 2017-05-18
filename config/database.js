8f654b6b5b53a80bf42735367772d48bd930099f
Evan Helbig <ehelbig1@yahoo.com>
2017-05-18 10:00:36 -0400

    update db connection

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://ehelbig1:21@%qwer@ds021172.mlab.com:21172/heroku_jslnm1g7',
+    database: 'mongodb://ehelbig:21@%qwer@ds021172.mlab.com:21172/heroku_jslnm1g7',
     secret: 'mysecret'
 }
