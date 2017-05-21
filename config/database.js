015242579665bd0bc05b9adcd809e3c75e84a633
Evan Helbig <ehelbig1@yahoo.com>
2017-05-21 08:32:24 -0400

    updating db connection

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb://ehelbig1:ElBandido2125@ds021172.mlab.com:21172/heroku_jslnm1g7',
     secret: 'mysecret'
 }
