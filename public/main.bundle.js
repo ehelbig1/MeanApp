69068bdb7195129546230ca1262d1a549c221a5a
Evan Helbig <ehelbig1@yahoo.com>
2017-05-22 08:48:49 -0400

    updated heading for foursquare

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -875,7 +875,7 @@ module.exports = "        <section id=\"contact\">\n            <div class=\"con
 /***/ 180:
 /***/ (function(module, exports) {
 
-module.exports = "<h2 class=\"page-header\">Dashbord</h2>\n<p>Welcom to your dashboard</p>\n\n<form [formGroup]='form' (ngSubmit)='searchVenues(form.value)' novalidate>\n    <div class=\"form-group\">\n        <input formControlName='keyword' class=\"form-control\" type=\"text\" name=\"keyword\" placeholder=\"search for...\" required>\n    </div>\n    <input class=\"btn btn-primary\" type=\"submit\">\n</form>\n\n<div class=\"panel\" *ngFor='let result of results'>\n    <h1 class=\"panel-head\">{{ result.name }}</h1>\n    <p>{{ result.id }}</p>\n    <ul>Location: \n        <li>{{ result.location.street }}</li>\n        <li>{{ result.location.cityState }}</li>\n    </ul>\n</div>"
+module.exports = "<h2 class=\"page-header\">Dashbord</h2>\n<h4>Search by keyword for venues near you!</h4>\n<span class=\"text-info\">eg. Coffee</span>\n\n<form [formGroup]='form' (ngSubmit)='searchVenues(form.value)' novalidate>\n    <div class=\"form-group\">\n        <input formControlName='keyword' class=\"form-control\" type=\"text\" name=\"keyword\" placeholder=\"search for...\" required>\n    </div>\n    <input class=\"btn btn-primary\" type=\"submit\">\n</form>\n\n<div class=\"panel\" *ngFor='let result of results'>\n    <h1 class=\"panel-head\">{{ result.name }}</h1>\n    <p>{{ result.id }}</p>\n    <ul>Location: \n        <li>{{ result.location.street }}</li>\n        <li>{{ result.location.cityState }}</li>\n    </ul>\n</div>"
 
 /***/ }),
 
